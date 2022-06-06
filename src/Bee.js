8826761875cd26a27538bc7897bc74dc8296bfaa
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-06-06 11:14:42 -0700

    refactored Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,14 +1,21 @@
 var Bee = function() {
   // THIS = Object.create(Grub.prototype);
-  Grub.call(this); // in the background line 2 and line 7 is happening
+
+  Grub.call(this);// in the background line 2 and line 9 is happening
   this.age = 5;
   this.color = 'yellow';
   this.job = 'keep on growing';
+
   // Return this;
 };
 
+// points to Grub methods
 Bee.prototype = Object.create(Grub.prototype);
+
+// points back to the constuctor
 Bee.prototype.constructor = Bee;
+
+// created bee methods
 Bee.prototype.eat = function() {
 
 };
