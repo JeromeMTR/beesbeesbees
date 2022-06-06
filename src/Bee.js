c393aefbcd2035c9453e2aaf28e62745a2350d2e
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-06-06 11:13:02 -0700

    completed Grub.js, Bee.js, HoneyMakerBee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,14 @@
 var Bee = function() {
+  // THIS = Object.create(Grub.prototype);
+  Grub.call(this); // in the background line 2 and line 7 is happening
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+  // Return this;
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+Bee.prototype.eat = function() {
+
+};
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+
+};
+// points to Bee & Grub methods
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+// points back to the constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+// creaate HoneyMakerBee Methods
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
 };
 
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
